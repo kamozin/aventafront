66fb91e69e64f5f46e8460872fe0185735dc11c9
Денис <corvus007@yandex.ru>
2017-04-09 14:51:05 +0300

    Обновление сайта

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('script.js');
+  svg4everybody(); // run it now or whenever you are ready
+
+  /*=================================
+  =            Billboard            =
+  =================================*/
+
+  var billboardSlider = document.querySelector('.billboard__slider');
+  if (billboardSlider) {
+    $(billboardSlider).slick({
+      accessibility: false,
+      // autoplay: true,
+      // arrows: false
+    });
+  }
+
+  /*=====  End of Billboard  ======*/
+
+
+
+  /*====================================
+  =            Inline popup            =
+  ====================================*/
+
+  $('.js-trigger-inline-popup').magnificPopup({
+    mainClass: 'popup-fade',
+    removalDelay: 300
+  });
+
+  /*=====  End of Inline popup  ======*/
+
+
+
+  /*==================================
+  =            Input mask            =
+  ==================================*/
+
+  $('input[type="tel"]').mask("+7 (999) 999-99-99", {});
+
+  /*=====  End of Input mask  ======*/
+
 });
