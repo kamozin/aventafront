14e74abd20d37b3c983e30c09c103ac10296b83f
Денис <corvus007@yandex.ru>
2017-04-13 23:20:34 +0300

    Добавляет функционал для меню каталога и иконки-гамбургера

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-  svg4everybody(); // run it now or whenever you are ready
+  svg4everybody();
 
   var main = document.querySelector('.main');
   var mainNav = document.querySelector('.main-nav');
+  var mainNavToogle = mainNav.querySelector('.main-nav__toggle');
+  var mainNavList = mainNav.querySelector('.main-nav__list');
 
   mainNav.addEventListener('mouseover', toggleMainNavOverlay);
   mainNav.addEventListener('mouseout', toggleMainNavOverlay);
 
-  function toggleMainNavOverlay() {
-    main.classList.toggle('main--overlay');
+  function toggleMainNavOverlay(event) {
+    if (mainNav.classList.contains('main-nav--opened')) {
+      main.classList.toggle('main--overlay');
+    }
   }
+
+  mainNavToogle.addEventListener('click', function(event) {
+    event.preventDefault();
+    mainNav.classList.toggle('main-nav--opened');
+    main.classList.toggle('main--overlay');
+    mainNavToogle.classList.toggle('is-active');
+  });
 
   /*=================================
   =            Billboard            =
